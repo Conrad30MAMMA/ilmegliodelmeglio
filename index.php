30ab2bc9e622aaff519a127c36cde27f2b2a17de
Conrad30MAMMA <a30vizi@yahoo.it>
2025-02-22 13:06:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>  
+	<link rel="shortcut icon" href="ico/SS.ico" type="image/x-icon">
+
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            font-family: 'Arial', sans-serif; 
+        }
+        header {
+            text-align: center;
+            margin-top: 20px;
+        }
+        nav ul {
+            display: flex;
+            list-style-type: none;
+            padding: 0;
+        }
+        nav ul li {
+            margin-right: 20px;
+        }
+        main {
+            display: flex;
+            justify-content: space-between;
+            width: 80%;
+        }
+        article {
+            flex: 3;
+        }
+        aside {
+            flex: 1;
+            margin-left: 5px;
+        }
+        aside figure{
+        	border: 2px solid #000;
+            padding: 0px;
+            margin-bottom: 0px;
+            text-align: center;
+            box-shadow: 3px 3px 5px #888888;
+            transition: all 2s;
+        }
+        aside figure:active {
+        	box-shadow: inset 3px 3px 5px #888888;
+        }
+        aside img {
+            width: 80%;
+            height: auto; /* Aggiunge un contorno intorno alle figure dell'aside */
+        }
+
+        /* Aggiungi questo blocco per l'animazione pulsante */
+        @keyframes pulsate {
+            0% {
+                opacity: 0;
+            }
+             100% {
+                opacity: 1;
+            }
+        }
+        .pulsating-text {
+            color: aqua;
+            font-weight: bold;
+            animation: pulsate 1s infinite;
+        }
+
+    </style>
+    
+</head>
+
+<body>
+
+<header>
+    <h1 class="pulsating-text">IN COSTRUZIONE</h1>
+    <h1 class="pulsating-text">FLASH30</h1>
+    <h2>La Matematica e la Fisica semplificate!!</h2>
+</header>
+
+<nav>
+    <ul>
+        <li><a href="../index.php">Home</a></li>
+        <li><a href="/imm_quiz/immissione.php">Immissione DATI</a></li>
+        <li><a href="/progetto/progetto.php">PROGETTO</a></li>
+        <li><a href="/quiz/quiz.php">QUIZ FISICA</a></li>
+    </ul>
+</nav>
+
+<main>
+
+<article>
+
+   <h2>Importanza della Matematica e della Fisica</h2>
+
+<p>La Matematica e la Fisica sono le colonne portanti del progresso moderno. 
+Queste discipline non solo ci permettono di comprendere il mondo che ci circonda, 
+ma sono anche fondamentali per il progresso tecnologico e il benessere della società. 
+La Matematica, con le sue formule e teoremi, ci offre gli strumenti per risolvere problemi complessi e 
+prendere decisioni informate. La Fisica, d'altra parte, ci aiuta a comprendere le leggi fondamentali 
+dell'universo, dalle particelle subatomiche alle lontane galassie.</p>
+
+<p>Nel contesto dell'ordinamento scolastico italiano, è essenziale che queste materie siano insegnate in 
+modo approfondito ma anche chiaro e accessibile.</p>
+
+<h3>Suddivisione dell'Ordinamento Scolastico Italiano</h3>
+
+<ul>
+    <li><strong>Scuola dell'infanzia (Asilo)</strong>:
+        <ul>
+            <li><strong>Età</strong>: 3-6 anni</li>
+            <li><strong>Descrizione</strong>: Prevede attività ludiche e didattiche per favorire lo 
+            sviluppo cognitivo, emotivo e sociale dei bambini.</li>
+        </ul>
+    </li>
+    <li><strong>Scuola primaria (Elementari)</strong>:
+        <ul>
+            <li><strong>Età</strong>: 6-11 anni</li>
+            <li><strong>Descrizione</strong>: Fornisce le basi dell'istruzione, con materie come italiano, 
+            matematica, scienze, storia e geografia.</li>
+        </ul>
+    </li>
+    <li><strong>Scuola secondaria di primo grado (Medie)</strong>:
+        <ul>
+            <li><strong>Età</strong>: 11-14 anni</li>
+            <li><strong>Descrizione</strong>: Approfondisce le conoscenze acquisite nella scuola primaria 
+            e introduce nuove materie come lingue straniere e tecnologia.</li>
+        </ul>
+    </li>
+    <li><strong>Scuola secondaria di secondo grado (Superiori)</strong>:
+        <ul>
+            <li><strong>Età</strong>: 14-19 anni</li>
+            <li><strong>Descrizione</strong>: Si suddivide in vari indirizzi (liceo, istituto tecnico, 
+            istituto professionale) e prepara gli studenti per l'università o il mondo del lavoro.</li>
+        </ul>
+    </li>
+    <li><strong>Università</strong>:
+        <ul>
+            <li><strong>Età</strong>: 19+ anni</li>
+            <li><strong>Descrizione</strong>: Offre corsi di laurea triennale, magistrale e dottorati di 
+            ricerca in vari campi del sapere.</li>
+        </ul>
+    </li>
+</ul>
+
+<p>È cruciale che la Matematica e la Fisica siano insegnate in modo che gli studenti possano comprendere e 
+apprezzare la profondità di queste materie. Tuttavia, è altrettanto importante che l'insegnamento goda di 
+trasparenza e massima diffusione. Questo può essere ottenuto attraverso l'uso di esempi pratici, esperimenti 
+e applicazioni reali che rendano le teorie astratte più tangibili.</p>
+
+<p>In conclusione, la Matematica e la Fisica sono fondamentali per il progresso e il benessere della società. 
+È essenziale che queste materie siano insegnate in modo approfondito ma anche chiaro e accessibile, per 
+garantire che gli studenti possano comprendere e apprezzare la loro importanza. L'ordinamento scolastico 
+italiano offre una struttura ben definita per l'insegnamento di queste materie, ma è necessario un continuo 
+sforzo per migliorare la qualità dell'insegnamento e renderlo più efficace.</p>
+
+<p><em><strong>Quanto segue, fino alla fine della pagina, è tratto da Wikipedia:</strong></em></p>
+
+<p>
+Secondo diverse fonti online, l'istruzione in Italia potrebbe risultare <em><strong>particolarmente stressante</strong></em>. 
+Alcune statistiche arrivano ad affermare che circa tre studenti su quattro soffrano di stress per colpa 
+della scuola e che circa il 15% degli studenti universitari italiani abbiano ricevuto una <em>diagnosi di 
+disturbi mentali</em>. Da ciò sono state mosse alcune critiche riguardo:</p>
+<ul>
+    <li>l'efficacia dei compiti a casa sulla preparazione degli studenti, per cui sono state avanzate 
+    corrispondenti proposte sull'abolizione di essi;</li>
+    <li>l'incidenza della qualità dell'istruzione sulla salute psicofisica, tema affrontato da alcuni 
+    pedagogisti che hanno rilevato che i compiti a casa possono essere associati a depressione e disturbi 
+    del sonno;</li>
+    <li>riduzione del tempo per coltivare le attività extrascolastiche, con conseguente impatto sociale;</li>
+    <li>il fenomeno dell'abbandono scolastico in Italia che, secondo alcuni dati, risulta piuttosto esteso 
+    a causa del mancato sostegno da parte degli insegnanti;</li>
+    <li>la qualità dell'istruzione italiana rispetto ad altri Paesi, argomento trattato all'estero e che 
+    vedrebbe il <em><strong>sistema scolastico in Italia come tossico e antiquato</strong></em> e con enormi differenze in confronto 
+    al resto dell'Europa.</li>
+</ul>
+
+</article>
+
+<aside>
+    <h2 id="su">INDICE DI MATEMATICA (per settore):</h2><br>
+     
+<figure>        
+        <a href="/_elementari/_indiceE.php">
+        <img src="img/elementari1.jpg" alt="Disegno Beatrice alle elementari" width="400" height="200" title="DISEGNO fatto da BEATRICE: Indice Argomenti">
+         <figcaption>ELEMENTARI</figcaption>
+        </a> 
+</figure><br>
+        
+<figure>        
+        <a href="/_medie/_indiceM.php">
+        <img src="img/medie.jpg" alt="Indice Argomenti Medie" width="400" height="200" title="Indice Argomenti Medie">
+         <figcaption>MEDIE</figcaption>
+        </a>        
+</figure><br> 
+        
+<figure>        
+        <a href="/_superiori/_indiceS.php">
+        <img src="img/GRSuperSella.jpg" alt="Indice Argomenti Superiori" width="400" height="200" title="Indice Argomenti Superiori">
+         <figcaption>SUPERIORI</figcaption>
+        </a>        
+</figure><br> 
+ 
+ <figure>        
+        <a href="/_universita/_indiceU.php">
+        <img src="img/uni.svg" alt="Indice Argomenti Università" width="400" height="200" title="Indice Argomenti Università">
+         <figcaption>UNIVERSITA'</figcaption>
+        </a>        
+</figure><br> 
+       
+</aside>
+
+
+</main>
+
+<footer>
+<p><a href="Insegnamento_MATEMATICA.pdf">Sitografia su INSEGNAMENTO DELLA MATEMATICA NEL MONDO</a></p>
+   
+</footer>
+
+</body>
+</html>
